f801a30cce045a14b8f6ca4b829d596f2fec5edd
gaohui01 <gaohui01@5i5j.com>
2021-05-20 09:36:58 +0800

    Initial commit

---
+++ app/src/main/java/com/gh/runtimeonly_databinding_plugin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gh.runtimeonly_databinding_plugin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
